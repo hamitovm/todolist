2f19308e7a8e2d58071d13f0b55fa75dd9824e3f
hamitovm <hamitovm@gmail.com>
2022-11-26 14:28:59 +0300

    15l save 1. redundant files removed, files reorganized into folders.

--- src/components/EditableSpan/EditableSpan.tsx
+++ src/components/EditableSpan/EditableSpan.tsx
@@ -47,8 +47,6 @@ export const EditableSpan = React.memo((props: EditableSpanPropsType) => {
                                      // className={error ? 'error' : ''}
                                          helperText={error}
                                          label={'Type value'}
-
-
         />
     }
     return (
